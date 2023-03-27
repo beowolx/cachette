4c047e346a3f84e969c412496e25954a956c0d97
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2023-03-27 07:56:00 +0200

    feat(cli): implement cli commands

---
+++ src/cli.rs
@@ -0,0 +1,50 @@
+use crate::chunk_type::ChunkType;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "cahette")]
+#[command(bin_name = "cachette")]
+#[command(about = "A program to hide secret messages on PNG files", long_about = None)]
+pub struct Cli {
+  #[command(subcommand)]
+  pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+  /// Encode a message in a PNG file
+  Encode {
+    /// The PNG file to encode
+    input: std::path::PathBuf,
+
+    /// The message to encode
+    message: String,
+
+    /// The chunk type to use
+    #[clap(short, long, default_value = "tEXt")]
+    chunk_type: ChunkType,
+  },
+  /// Decode a message in a PNG file
+  Decode {
+    /// The PNG file to decode
+    input: std::path::PathBuf,
+
+    /// The chunk type to use
+    #[clap(short, long, default_value = "tEXt")]
+    chunk_type: ChunkType,
+  },
+  /// Remove a message from a PNG file
+  Remove {
+    /// The PNG file to remove the message from
+    input: std::path::PathBuf,
+
+    /// The chunk type to use
+    #[clap(short, long, default_value = "tEXt")]
+    chunk_type: ChunkType,
+  },
+  /// Print a message from a PNG file
+  Print {
+    /// The PNG file to print the message from
+    input: std::path::PathBuf,
+  },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
+use clap::Parser;
+use cli::{Cli, Commands};
+
 mod chunk;
 mod chunk_type;
+mod cli;
 mod png;
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-  todo!()
+  let args = Cli::parse();
+  match args.command {
+    Commands::Encode {
+      input,
+      message,
+      chunk_type,
+    } => {
+      todo!();
+    }
+    Commands::Decode { input, chunk_type } => {
+      todo!();
+    }
+    Commands::Remove { input, chunk_type } => {
+      todo!();
+    }
+    Commands::Print { input } => {
+      todo!();
+    }
+    _ => {
+      panic!("Unknown command");
+    }
+  }
 }
